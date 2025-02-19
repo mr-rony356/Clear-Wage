5a189516d03d944ed35d8129fc206ec701c5e968
Omor Faruk Rony <omorfarukrony2@gmail.com>
2025-02-19 22:33:51 +0600

    mobile height for hero

--- src/components/sections/MainSection.jsx
+++ src/components/sections/MainSection.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import { styled } from '@mui/system';
+import React from "react";
+import Box from "@mui/material/Box";
+import { styled } from "@mui/system";
 
-const MiuiSection = styled(Box)({
-  background: 'linear-gradient(rgb(37 37 37 / 90%), rgb(10 14 19 / 4%)), url(/bg1.jpg) 0% no-repeat',
-      backgroundSize: 'cover',
-    minHeight:'50vh',
-    position:'relative'
-});
+const MiuiSection = styled(Box)(({ theme }) => ({
+  background:
+    "linear-gradient(rgb(37 37 37 / 90%), rgb(10 14 19 / 64%)), url(/bg1.jpg) 50% no-repeat",
+  backgroundSize: "cover",
+  minHeight: "50vh",
+  position: "relative",
+  [theme.breakpoints.down("sm")]: {
+    minHeight: "70vh",
+  },
+}));
 
 function MainSection({ children }) {
   return <MiuiSection>{children}</MiuiSection>;
